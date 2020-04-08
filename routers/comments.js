9d1455eb8b8b1ccf8c623dd916327287ade7d6c8
John <cjd8801@gmail.com>
2020-04-08 14:59:15 -0500

    update

--- routers/comments.js
+++ routers/comments.js
@@ -5,20 +5,8 @@ const router = express.Router({
     mergeParams: true
 })
 
-// ### Comment Schema 
-//
-// A Comment in the database has the following structure:
-//
-// ```js
-// {
-//   text: "The text of the comment", // String, required
-//   post_id: "The id of the associated post", // Integer, required, must match the id of a post entry in the database
-//   created_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
-//   updated_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
-// }
-// ```
 
-// get (/api/posts/:id/comments) returns array of all comment objects associated with the post with the specified id
+
 router.get("/", (req, res) => {
     const id = req.params.id;
 
@@ -31,7 +19,7 @@ router.get("/", (req, res) => {
         })
 })
 
-// get (/api/posts/:id/comments/:commentId) returns specific comment by id
+
 
 router.get("/:commentId", (req, res) => {
     db.findCommentById(req.params.id, req.params.commentId)
@@ -51,7 +39,7 @@ router.get("/:commentId", (req, res) => {
         })
 })
 
-// post (/api/posts/:id/comments) creates a comment for the post with the specified id using the infromation sent inside of the request
+
 
 router.post("/", (req, res) => {
     const comment = req.body;
--- routers/posts.js
+++ routers/posts.js
@@ -6,20 +6,7 @@ const router = express.Router();
 
 router.use("/:id/comments", commentsRouter);
 
-// ### Blog Post Schema
 
-// A Blog Post in the database has the following structure:
-
-// ```js
-// {
-//   title: "The post title", // String, required
-//   contents: "The post contents", // String, required
-//   created_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
-//   updated_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
-// }
-// ```
-
-// get (/api/posts) returns an array of all the post objects
 router.get("/", (req, res) => {    
     db.find()
         .then(posts => {
@@ -31,7 +18,7 @@ router.get("/", (req, res) => {
         })
 })
 
-// get (/api/posts/:id) returns the post object with the specified id
+
 router.get("/:id", (req, res) => {
     const id = req.params.id;
 
@@ -49,7 +36,7 @@ router.get("/:id", (req, res) => {
         })
 })
 
-// post (/api/posts) creates a post using the info sent inside the request body
+/
 router.post("/", (req, res) => {
     if(!req.body.title || !req.body.contents) {
         return res.status(400).json({ errorMessage: "Please provide title and contents for the post" })
@@ -65,8 +52,7 @@ router.post("/", (req, res) => {
         })
 })
 
-// put (/api/posts/:id) updates the post with the specified id using data from the request body. 
-// Returns modified document, not the original.
+
 router.put("/:id", async (req, res) => {
     const id = req.params.id;
     const { title, contents } = req.body;
@@ -90,7 +76,7 @@ router.put("/:id", async (req, res) => {
     }
 })
 
-// delete (/api/posts/:id) removes the post with the specified id and returns the deleted post object
+
 router.delete("/:id", (req, res) => {
     db.remove(req.params.id)
       .then(count => {
